3e07fc553e6121885fac01ff799d611d5efce861
Özgür Isbert <oezguerisbert@gmail.com>
2021-03-09 15:14:25 +0100

    fix: migration file fix

--- modul-154.sh
+++ modul-154.sh
@@ -1,2 +1,2 @@
 docker run -d --name "modul-154_2" -p 3307:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql
-docker exec -it "modul-154_2" /bin/bash -c "$(cat migration-docker.sh)"
+docker exec -it "modul-154_2" /bin/bash -c "$(cat ./migration-docker.sh)"
