66b7bbb7b5b7cd5a9a51a18ca207291e127fefbc
Özgür Isbert <oezguerisbert@gmail.com>
2021-03-09 13:48:33 +0100

    fix: docker preparation in container got fixed

--- modul-154.sh
+++ modul-154.sh
@@ -1,2 +1,2 @@
 docker run -d --name modul-154_2 -p 3307:3306 -e MYSQL_ALLOW_EMPTY_PASS
-WORD=true mysql "apt update && apt upgrade -y && apt install git -y; mkdir mysql_files && git clone https://github.com/oezguerisbert/modul-154.git && cd modul-154 && mysql -u root employee < employee.sql"
+WORD=true mysql "apt update && apt upgrade -y && apt install git -y; mkdir mysql_files && cd mysql_files && git clone https://github.com/oezguerisbert/modul-154.git && cd modul-154 && mysql -u root --execute "create database employee" && mysql -u root employee < employee.sql"
