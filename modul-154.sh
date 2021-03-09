be68d179ae51cab281db2e47f2faa103c398e3c5
Özgür Isbert <oezguerisbert@gmail.com>
2021-03-09 14:59:59 +0100

    refactor: refactored /bin/bash command

--- modul-154.sh
+++ modul-154.sh
@@ -1,2 +1,2 @@
 docker run -d --name "modul-154_2" -p 3307:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql
-docker exec -it "modul-154_2" /bin/bash -c "apt update && apt install git -y && git clone https://github.com/datacharmer/test_db.git && cd test_db && mysql -u root --execute 'create database employees; create database employee;' && mysql -u root employees < employees.sql && cd .. && git clone https://github.com/oezguerisbert/modul-154.git && cd modul-154 && mysqldump -u root employees > emp.sql && mysql -u root employee < emp.sql && mysql -u root employee < migration.sql"
+docker exec -it "modul-154_2" /bin/bash -c "$(cat migration-docker.sh)"
